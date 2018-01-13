c5a4d489a756cfb53e82025796b570e5fbe76e6a
21350010 <32065584+21350010@users.noreply.github.com>
2018-01-13 21:32:14 +0000

    session10.1a.1py updated

---
+++ Session10.1a.1.py
@@ -0,0 +1,17 @@
+
+class Student:
+    def __init__(self,FirstName,LastName,StudentNo,Course):
+        self.first=FirstName
+        self.last=LastName
+        self.stdno=StudentNo
+        self.course=Course
+
+    def studentInfo(self):
+        print(self.FirstName,self.LastName,self.StudentNo,self.Course)
+
+         
+
+
+
+
+  
